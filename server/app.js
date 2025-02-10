d4ffcde95b7f96e1b28d9b38d554b066603cbdf9
Paul Wentzel <paul.wentzel@gmail.com>
2025-02-10 15:43:25 -0500

    bonus complete

--- server/app.js
+++ server/app.js
@@ -25,6 +25,39 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Body:', req.body);
+  next();
+});
+
+app.get("/artists/latest", (req, res) => {
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json');
+  res.json(getLatestArtist());
+  res.send();
+});
+
+app.get("/artists/latest/albums", (req, res) => {
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json');
+  res.json(getAlbumsForLatestArtist());
+  res.send();
+});
+
+app.get("/artists", (req, res) => {
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json');
+  res.json(getAllArtists());
+  res.send();
+});
+
+app.post("/artists", (req, res) => {
+  res.status(201);
+  res.setHeader('Content-Type', 'application/json');
+  res.json(addArtist(req.body));
+  res.send();
+});
 
 // DO NOT MODIFY
 if (require.main === module) {
